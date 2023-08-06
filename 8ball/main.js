de270d9a264b4df0b8e9d1d40dd636fe3c3cbcaf
jkmunoz <juliannakmunoz@gmail.com>
2023-08-06 14:07:17 -0500

    changes to log for cleaner output.

--- 8ball/main.js
+++ 8ball/main.js
@@ -4,46 +4,46 @@ var userName = 'Julianna';
 var userQuestion = 'Will my tikTok video go viral?';
 // Random number between 0 and 7.
 var randomNumber = Math.floor(Math.random() * 8);
-// Random eightball message. 
+// Random eightball message, chosen by the random number. 
 var eightBall = randomNumber;
 
 // This generates two seperate responses depending on if a username is given or not.).
 if (userName) {
-    console.log(`Hello, ${userName}`);
+    console.log(`Hello, ${userName}!`);
 } else {
     console.log('Hello!');
 }
 
 // This will pring after the user gives us their name nad question. 
 // It returns the question back. 
-console.log(`Ah! I see ${userName}, you want to know ${userQuestion}. 
-Let's have a look...`)
+console.log(`${userName} wants to know "${userQuestion}"
+Let's see...`)
 
 // Switch statement that pairs a response with a random number. 
 console.log(`*Shakes eightball*`);
 switch (eightBall) {
     case 0:
-        console.log('For sure, my guy!.');
+        console.log('Response: For sure, my guy!');
         break;
     case 1:
-        console.log('Bet. No Cap. On God.');
+        console.log('Response: Bet. No Cap. On God.');
         break;
     case 2:
-        console.log('Go for it Sis!');
+        console.log('Response: Go for it Sis!');
         break;
     case 3:
-        console.log("Get those W's");
+        console.log("Response: Get those W's");
         break;
     case 4:
-        console.log("This ain't it cheif.");
+        console.log("response: This ain't it cheif.");
         break;
     case 5:
-        console.log("You future is giving L's.");
+        console.log("Response: You future is giving L's.");
         break;
     case 6:
-        console.log('Outlook is mid.');
+        console.log('response: Outlook is mid.');
         break;
     case 7:
-        console.log("It's giving sus vibes.");
+        console.log("response: It's giving sus vibes.");
         break;
 }
